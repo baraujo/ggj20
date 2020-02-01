f0d8c75fb9e5ed469b449937cac16578a9618d23
Bruno Araújo <bruno@baraujo.net>
2020-02-01 00:24:21 -0400

    Inititial movement and wheel interaction

---
+++ Assets/HouseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + Vector3.right * 0.25f * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class PlayerController : MonoBehaviour
+{
+    // Movement data
+    [SerializeField] private float m_MoveSpeed;
+    private Vector3 m_Velocity;
+    private Vector3 m_VelocityRef;
+    private Vector2 m_NormalizedVelocity;
+    private Rigidbody2D m_Rigidbody;
+    
+    // Collision data
+    private WheelController m_CurrentWheelController = null;
+    private WallController m_CurrentWallController = null;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //var newPosition = Vector3.SmoothDamp(transform.position, transform.position + m_Velocity * Time.fixedDeltaTime, ref m_VelocityRef, 0.05f);
+        m_Rigidbody.MovePosition(transform.position + m_Velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Wheel")) return;
+        
+        Debug.Log($"{name} Entered wheel collider {other.name}");
+        m_CurrentWheelController = other.GetComponent<WheelController>();
+        if (m_CurrentWheelController.currentPlayer == null)
+        {
+            m_CurrentWheelController.currentPlayer = this;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Wheel")) return;
+        
+        Debug.Log($"{name} left wheel collider {other.name}");
+        // TODO: Stop action on wheel if any
+        m_CurrentWheelController = null;
+    }
+
+    public void Move(InputAction.CallbackContext value)
+    {
+        m_NormalizedVelocity = value.ReadValue<Vector2>() * m_MoveSpeed;
+        m_Velocity.x = m_NormalizedVelocity.x;
+        m_Velocity.y = m_NormalizedVelocity.y;
+    }
+
+    public void Action(InputAction.CallbackContext value)
+    {
+        if (!value.performed) return;
+        if (!(value.control is ButtonControl button)) return;
+
+        if (m_CurrentWheelController != null)
+        {
+            ProcessWheelAction(button.isPressed);
+        }
+        else if (m_CurrentWallController != null)
+        {
+            ProcessWallAction(button.isPressed);
+        }
+    }
+
+    private void ProcessWheelAction(bool buttonIsPressed)
+    {
+        if (buttonIsPressed)
+        {
+            if (m_CurrentWheelController.currentPlayer == null)
+            {
+                Debug.Log("m_CurrentWheelController.currentPlayer is null on press Action, this shouldn't happen");
+            }
+            else if(m_CurrentWheelController.currentPlayer == this)
+            {
+                Debug.Log($"{name} started using action on wheel collider {m_CurrentWheelController.name}");  
+                // TODO: perform wheel action
+            }
+            else
+            {
+                Debug.Log($"Cannot use action, Player {m_CurrentWheelController.currentPlayer.name} is inside wheel collider {m_CurrentWheelController.name}");
+            }
+        }
+        else
+        {
+            if (m_CurrentWheelController.currentPlayer == null)
+            {
+                Debug.Log("m_CurrentWheelController.currentPlayer is null on release Action, this shouldn't happen");
+            }
+            else if(m_CurrentWheelController.currentPlayer == this)
+            {
+                Debug.Log($"{name} stopped using action on wheel collider {m_CurrentWheelController.name}");
+                // TODO: stop wheel action
+            }
+            else
+            {
+                Debug.Log($"Cannot stop action, Player {m_CurrentWheelController.currentPlayer.name} is inside wheel collider {m_CurrentWheelController.name}");
+            }
+        }
+    }
+    private void ProcessWallAction(bool buttonIsPressed)
+    {
+        throw new NotImplementedException();
+    }
+}
