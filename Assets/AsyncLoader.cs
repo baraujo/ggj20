c633d2bc98140e3ecc8b4d4807d707d02cb6a76f
Bruno Araújo <bruno@baraujo.net>
2020-01-31 21:20:42 -0400

    Initial commit

---
+++ Assets/AsyncLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AsyncLoader : MonoBehaviour
+{
+    public AssetReference sceneReference;
+    private void Start()
+    {
+        Addressables.LoadSceneAsync(sceneReference);
+    }
+}
